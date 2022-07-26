4261f95c72ef95e9ceaae413231221b7b6b39d9c
Lio <liomazza@gmail.com>
2022-07-26 14:39:47 -0300

    setup porject and setup room sql database

---
+++ app/src/main/java/com/example/lio/noteme/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.lio.noteme.feature_note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.lio.noteme.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NoteDatabase: RoomDatabase() {
+
+    abstract val noteDao: NotesDao
+}
