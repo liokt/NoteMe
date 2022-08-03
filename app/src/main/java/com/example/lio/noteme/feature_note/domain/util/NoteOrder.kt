00f22cdaaf84ebbe7fab2d81f26771081af2ee2a
Lio <liomazza@gmail.com>
2022-08-03 10:41:56 -0300

    composables components & notes screen composable

--- app/src/main/java/com/example/lio/noteme/feature_note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/example/lio/noteme/feature_note/domain/util/NoteOrder.kt
@@ -1,9 +1,15 @@
 package com.example.lio.noteme.feature_note.domain.util
 
-import com.example.lio.noteme.feature_note.domain.model.Note
-
 sealed class NoteOrder(val orderType: OrderType) {
     class Title(orderType: OrderType): NoteOrder(orderType)
     class Date(orderType: OrderType): NoteOrder(orderType)
     class Color(orderType: OrderType): NoteOrder(orderType)
+
+    fun copy(orderType: OrderType): NoteOrder {
+        return when(this) {
+            is Title -> Title(orderType)
+            is Date -> Date(orderType)
+            is Color -> Color(orderType)
+        }
+    }
 }
